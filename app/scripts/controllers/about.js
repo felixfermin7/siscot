383bc58eaffd79789208b2306672ee84d413f8f6
felixfermin7 <felixfermin7@gmail.com>
2017-07-01 00:48:34 -0400

    felix

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name siscotApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the siscotApp
+ */
+angular.module('siscotApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
